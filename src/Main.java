450025b34f9758ccd5d9a5d42e8fad852b39ece6
nsvintitskih <nsvintitskih@yandex.ru>
2023-10-18 00:08:56 +0300

    DzLisson6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] mas = new int[50];
+        // библиотека метод рендум дабл от 0 до1
+        for(int i =0; i<mas.length; i++){
+            mas[i] =(int) (Math.random()*100-50);//это отриц числа, полож просто умнож на 50
+        }
+        for(int item: mas){// перебор всех элеентов в цикле
+            System.out.println(item + " ");
+        }
+        int pos = 0;
+        int neg = 0;
+        int zero =0;
+        int sumPos = 0;
+        int sumNeg = 0;
+        int averge;
+
+        for (int item : mas){
+            if (item>0){
+                pos++;
+                sumPos = sumPos + item;
+            }else if(item<0){
+                neg++;
+                sumNeg = sumNeg + item;
+            }else{
+                zero++;
+
+            }
+
+        } averge = (sumPos + sumNeg)/50;
+
+        System.out.println("Info");
+        System.out.println("+:" + pos);
+        System.out.println("-:" + neg);
+        System.out.println("0:" + zero);
+        System.out.println("SumPos:" + sumPos);
+        System.out.println("SumNeg:" + sumNeg);
+        System.out.println("Averge:" + averge);
     }
+
 }
